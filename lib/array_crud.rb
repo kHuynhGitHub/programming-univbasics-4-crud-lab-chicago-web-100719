c243e1619eb4719c5cfda50227d1087122f1252d
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-24 02:58:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   [1, 2, 3, 4]
 end
 
-def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array.push[element]
+def add_element_to_end_of_array[array, element]
+  add_element_to_end_of_array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
