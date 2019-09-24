42f13ae796d38bc67a8914cc0fdef2e9e2105eec
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-24 03:00:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,29 +11,29 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array.unshift
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array.pop
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  remove_element_from_start_of_array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index[index_number]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  retrieve_last_element_from_array[-1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  update_element_from_index[index_number] = "element"
+  array[index_number] = "element"
 end
