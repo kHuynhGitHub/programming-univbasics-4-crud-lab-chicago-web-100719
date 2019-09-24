2356c28685e1b1606ba8b854b09ae1fb885f7a24
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-24 02:42:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,15 +23,15 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index[]
+  retrieve_element_from_index[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  retrieve_last_element_from_array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
